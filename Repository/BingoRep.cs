37c6803ee80b7cbebe260f3be9a177a2c12fca21
LuisSaudino <47904749+LuisSaudino@users.noreply.github.com>
2021-09-13 22:28:26 -0300

    Add files via upload

---
+++ Repository/BingoRep.cs
@@ -0,0 +1,58 @@
+﻿using Cbm.BaseRazor.Helpers;
+using Compartilhado.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Repository
+{
+    public class BingoRep : IRepositoryBingo
+    {
+        private IHttpService _httpService;
+
+        private String baseUrl = "/api/Bingo";
+
+        public BingoRep(IHttpService httpService)
+        {
+            _httpService = httpService;
+        }
+        public int Id { get; set; } = 1;
+
+        public async Task<List<int>> GetNumerosSorteados()
+        {
+            HttpResponseWrapper<List<int>> retorno = await _httpService.Get<List<int>>(baseUrl + @"/numerosSorteio");
+            if (retorno.Sucess)
+            {
+                return retorno.Response;
+            }
+            throw new NotImplementedException();
+
+        }
+
+        public async Task<int> SortearPedra()
+        {
+            var retorno = await _httpService.Post<Object, int>(baseUrl + @"/", null);
+
+            if (retorno.Sucess)
+                return retorno.Response;
+
+            return -1;
+        }
+
+        public bool VericarCartela(Cartela cartela)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<int> IRepositoryBingo.GetNumerosSorteados()
+        {
+            throw new NotImplementedException();
+        }
+
+        int IRepositoryBingo.SortearPedra()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Repository/CartelaRep.cs
@@ -0,0 +1,29 @@
+﻿using Cbm.BaseRazor.Helpers;
+using Compartilhado.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontEnd.Repository
+{
+    public class CartelaRep : IRepositoryCartela
+    {
+        private IHttpService _httpService;
+        private string baseUrl = @"/api/Cartela";
+
+        public CartelaRep(IHttpService httpService) : base()
+        {
+
+        }
+        public Usuario ComprarCartela(Usuario user, Cartela cartela)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cartela GenerateCartela()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
