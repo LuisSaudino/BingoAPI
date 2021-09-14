2d87a3f674bc06018e46898af5f1121228f647e3
LuisSaudino <47904749+LuisSaudino@users.noreply.github.com>
2021-09-13 22:26:46 -0300

    Add files via upload

---
+++ WebApiAulaSD/Controllers/BingoController.cs
@@ -0,0 +1,40 @@
+﻿using Compartilhado.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAulaSD.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BingoController : Controller
+    {
+        private FakeBingoRep _bingoHelper;
+
+        public BingoController() : base() {
+            _bingoHelper = new FakeBingoRep();
+        }
+
+        [HttpGet("numerosSorteio")]
+        public List<int> NumerosSorteados(int id_bingo)
+        {
+            return _bingoHelper.GetNumerosSorteados();
+        }
+
+        [HttpPost]
+        public void SortearPedra(int id_bingo)
+        {
+            _bingoHelper.SortearPedra();
+            return;
+        }
+
+        [HttpPost("Verificar")]
+        public bool VerificarCartela()
+        {
+            var retorno = _bingoHelper.VericarCartela(cartela);
+            return retorno;
+        }
+    }
+}
---
+++ WebApiAulaSD/Controllers/CartelaController.cs
@@ -0,0 +1,27 @@
+﻿using Compartilhado.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiAulaSD.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class CartelaController : Controller
+    {
+        private FakeCartelaRep _cartelaHelper;
+
+        public CartelaController() : base() {
+            _cartelaHelper = new FakeCartelaRep();
+
+        }
+
+        [HttpGet("NovaCartela")]
+        public async Task<ActionResult<Cartela>> Get()
+        {
+            return _cartelaHelper.GenerateCartela();
+        }
+    }
+}
